38b116490b0b4cdb8c24f30bd36f54e7f48541a8
MuazzezA <ayd.muazzez@gmail.com>
2023-08-19 22:08:56 +0300

    Change App.js

---
+++ LocationHeatMapper/src/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Location Heat Mapper</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
