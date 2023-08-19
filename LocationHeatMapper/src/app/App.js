c3758706ee3b5ada3e9115e9cd28757f31d0c00d
MuazzezA <ayd.muazzez@gmail.com>
2023-08-20 02:31:34 +0300

    Connected Firebase for Android integration

--- LocationHeatMapper/src/app/App.js
+++ LocationHeatMapper/src/app/App.js
@@ -4,6 +4,12 @@ import {Provider} from 'react-redux';
 import AppStack from './navigation/app-stack';
 import store from './redux/store';
 
+import firebase from '@react-native-firebase/app';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 const App = () => {
   return (
     <Provider store={store}>
