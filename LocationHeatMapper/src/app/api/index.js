eade2229cc8ff6777fc432101e262385f0033de1
MuazzezA <ayd.muazzez@gmail.com>
2023-08-20 12:26:24 +0300

    Create getCurrentPosition func api

--- LocationHeatMapper/src/app/api/index.js
+++ LocationHeatMapper/src/app/api/index.js
@@ -1,5 +1,6 @@
 import {firebase} from '@react-native-firebase/database';
 import {firebaseConfig} from '../api/firebase';
+import Geolocation from '@react-native-community/geolocation';
 
 export const getFirebaseData = setData => {
   const reference = firebase
@@ -22,5 +23,15 @@ export const getFirebaseData = setData => {
 export const pushFirebaseData = newdata => {
   const newReference = firebase.app().database().ref('/locations').push();
 
-  newReference.set(newdata).then(() => console.log('Data updated.'));
+  newReference.set(newdata).then(() => console.log('Data pushed.'));
+};
+
+export const getCurrentPosition = setPosition => {
+  Geolocation.getCurrentPosition(
+    pos => {
+      setPosition(pos);
+    },
+    error => Alert.alert('GetCurrentPosition Error', JSON.stringify(error)),
+    {enableHighAccuracy: true},
+  );
 };
