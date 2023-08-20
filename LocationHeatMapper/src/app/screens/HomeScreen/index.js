2b95604980e42ebaa47517bed7f7f28112f7b15d
MuazzezA <ayd.muazzez@gmail.com>
2023-08-20 21:53:41 +0300

    Add map to home screen

--- LocationHeatMapper/src/app/screens/HomeScreen/index.js
+++ LocationHeatMapper/src/app/screens/HomeScreen/index.js
@@ -1,19 +1,34 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, FlatList, ScrollView, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  ScrollView,
+  SafeAreaView,
+  Button,
+} from 'react-native';
 import {getCurrentPosition, getFirebaseData, pushFirebaseData} from '../../api';
+import MapView, {Heatmap, PROVIDER_GOOGLE} from 'react-native-maps';
+import styles from './styles';
+// import {HeatmapTileProvider} from 'react-native-maps';
+
+//position
+// {"coords": {"accuracy": 5, "altitude": 5, "heading": 0, "latitude": 37.421998333333335, "longitude": -122.084, "speed": 0},
+// "extras": {"maxCn0": 0, "meanCn0": 0, "satellites": 0}, "mocked": false, "timestamp": 1692542698046}
 
 export const HomeScreen = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   const [position, setPosition] = useState({});
-
+  const [region, setRegion] = useState();
+  const [processedData, setProcessedData] = useState([]);
   useEffect(() => {
     getFirebaseData(setData);
 
-    // her 1 dk da bir verileri çek
+    //her 1 dk da bir verileri çek
     const intervalGet = setInterval(() => {
       getFirebaseData(setData);
       console.log('get data');
-    }, 30000);
+    }, 60000);
 
     // her 10 sn de bir veri gönder
     const intervalPush = setInterval(() => {
@@ -22,23 +37,81 @@ export const HomeScreen = () => {
     }, 10000);
 
     return () => {
-      clearInterval(intervalGet);
+      // clearInterval(intervalGet);
       clearInterval(intervalPush);
     };
   }, []);
 
-  console.log('----');
+  useEffect(() => {
+    console.log('--------------------');
+    console.log('position : ', position);
+    if (
+      position &&
+      position.coords &&
+      position.coords.latitude !== undefined &&
+      position.coords.longitude !== undefined
+    ) {
+      console.log('position.coords : ', position.coords);
+      setRegion({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      });
+    }
+    console.log('--------------------');
+  }, [position]);
 
+  useEffect(() => {
+    const processed = Object.values(data).map(item => ({
+      latitude: item.coords.latitude,
+      longitude: item.coords.longitude,
+      weight: 1,
+    }));
+    setProcessedData(processed);
+  }, [data]);
+
+  console.log('processedData', processedData);
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Text
-        style={{
-          fontSize: 15,
-          fontWeight: 'bold',
-        }}>
-        {JSON.stringify(position)}
-      </Text>
-      <Text>data : {JSON.stringify(data)}</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Button title="get" onPress={() => getFirebaseData(setData)} />
+      {data ||
+      data !== null ||
+      data !== undefined ||
+      processedData ||
+      processedData !== null ||
+      processedData !== undefined ||
+      position ? (
+        <MapView
+          zoomEnabled={true}
+          zoomControlEnabled={true}
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          onRegionChange={reg => console.log('region on change : ', reg)}
+          region={
+            region
+              ? region
+              : {
+                  latitude: 37.8746,
+                  longitude: 32.4943,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }
+          }>
+          {processedData.length > 0 && (
+            <Heatmap
+              points={processedData}
+              opacity={0.8}
+              radius={50}
+              maxIntensity={100}
+              gradientSmoothing={10}
+              heatmapMode={'POINTS_DENSITY'}
+            />
+          )}
+        </MapView>
+      ) : (
+        <Text>bekle</Text>
+      )}
+    </View>
   );
 };
---
+++ LocationHeatMapper/src/app/screens/HomeScreen/styles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    marginTop: 10,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    height: '100%',
+    width: '100%',
+  },
+});
