ea0fa31e7f61567e6c3878e0834f9cdc924df5e5
MuazzezA <ayd.muazzez@gmail.com>
2023-08-21 10:09:38 +0300

    test home screen

--- LocationHeatMapper/src/app/screens/HomeScreen/index.js
+++ LocationHeatMapper/src/app/screens/HomeScreen/index.js
@@ -2,15 +2,21 @@ import React, {useEffect, useState} from 'react';
 import {
   View,
   Text,
-  FlatList,
-  ScrollView,
-  SafeAreaView,
-  Button,
+  TouchableOpacity,
+  PermissionsAndroid,
+  Platform,
+  Alert,
+  Linking,
 } from 'react-native';
 import {getCurrentPosition, getFirebaseData, pushFirebaseData} from '../../api';
-import MapView, {Heatmap, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {
+  Heatmap,
+  PROVIDER_GOOGLE,
+  PROVIDER_DEFAULT,
+} from 'react-native-maps';
 import styles from './styles';
-// import {HeatmapTileProvider} from 'react-native-maps';
+
+import Geolocation from '@react-native-community/geolocation';
 
 //position
 // {"coords": {"accuracy": 5, "altitude": 5, "heading": 0, "latitude": 37.421998333333335, "longitude": -122.084, "speed": 0},
@@ -21,7 +27,12 @@ export const HomeScreen = () => {
   const [position, setPosition] = useState({});
   const [region, setRegion] = useState();
   const [processedData, setProcessedData] = useState([]);
+  const [mapProvider, setMapProvider] = useState(PROVIDER_DEFAULT);
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
+    requestLocationPermission();
+
     getFirebaseData(setData);
 
     //her 1 dk da bir verileri çek
@@ -32,15 +43,88 @@ export const HomeScreen = () => {
 
     // her 10 sn de bir veri gönder
     const intervalPush = setInterval(() => {
-      getCurrentPosition(setPosition);
-      pushFirebaseData(position);
+      //getCurrentPosition(setPosition);
+
+      Geolocation.getCurrentPosition(
+        position => {
+          // Konum alındı, işlemleri burada yapabilirsiniz
+          setPosition(position);
+          setCount(count + 1);
+          pushFirebaseData(position);
+          Alert.alert(
+            'Konum ayarlandı -interval push-',
+            `${JSON.stringify(position)}`,
+          );
+        },
+        error => {
+          if (error.code === 1) {
+            // Kullanıcı konumu kapalı
+            Alert.alert(
+              'Konum Kapalı',
+              'Uygulamayı kullanabilmek için konumunuzu açmanız gerekmektedir.',
+              [
+                {
+                  text: 'Ayarlar',
+                  onPress: () => {
+                    // Ayarlara yönlendir
+                    if (Platform.OS === 'ios') {
+                      Linking.openURL('app-settings:');
+                    } else if (Platform.OS === 'android') {
+                      Linking.openSettings();
+                      //Linking.sendIntent('android.settings.LOCATION_SOURCE_SETTINGS');
+                    }
+                  },
+                },
+                {text: 'Vazgeç', style: 'cancel'},
+              ],
+              {cancelable: false},
+            );
+          } else {
+            Alert.alert('Bir hata oluştu', error.message);
+          }
+        },
+        {enableHighAccuracy: false, timeout: 20000, maximumAge: 1000},
+      );
+
+      //pushFirebaseData(position);
+
+      // getCurrentPosition(newPosition => {
+      //   setPosition(newPosition);
+      //   pushFirebaseData(newPosition); // Yalnızca konum alındığında pushFirebaseData çağrılır
+      // });
     }, 10000);
 
     return () => {
-      // clearInterval(intervalGet);
+      clearInterval(intervalGet);
       clearInterval(intervalPush);
     };
   }, []);
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Geolocation Permission',
+          message: 'Can we access your location?',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      console.log('granted', granted);
+      if (granted === 'granted') {
+        console.log('You can use Geolocation');
+        setMapProvider(PROVIDER_GOOGLE);
+        return true;
+      } else {
+        console.log('You cannot use Geolocation');
+        return false;
+      }
+    } catch (err) {
+      return false;
+    }
+  };
 
   useEffect(() => {
     console.log('--------------------');
@@ -74,7 +158,6 @@ export const HomeScreen = () => {
   console.log('processedData', processedData);
   return (
     <View style={styles.container}>
-      <Button title="get" onPress={() => getFirebaseData(setData)} />
       {data ||
       data !== null ||
       data !== undefined ||
@@ -82,33 +165,48 @@ export const HomeScreen = () => {
       processedData !== null ||
       processedData !== undefined ||
       position ? (
-        <MapView
-          zoomEnabled={true}
-          zoomControlEnabled={true}
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          onRegionChange={reg => console.log('region on change : ', reg)}
-          region={
-            region
-              ? region
-              : {
-                  latitude: 37.8746,
-                  longitude: 32.4943,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
-                }
-          }>
-          {processedData.length > 0 && (
-            <Heatmap
-              points={processedData}
-              opacity={0.8}
-              radius={50}
-              maxIntensity={100}
-              gradientSmoothing={10}
-              heatmapMode={'POINTS_DENSITY'}
-            />
-          )}
-        </MapView>
+        <>
+          <MapView
+            //onMapReady={() => console.log('map ready')}
+            zoomEnabled={true}
+            zoomControlEnabled={true}
+            //mapType={Platform.OS == 'android' ? 'none' : 'standard'}
+            //provider={mapProvider} // remove if not using Google Maps
+            style={styles.map}
+            //onRegionChange={reg => console.log('region on change : ', reg)}
+            region={
+              region
+                ? region
+                : {
+                    latitude: 37.8746,
+                    longitude: 32.4943,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                  }
+            }>
+            {processedData.length > 0 && (
+              <Heatmap
+                points={processedData}
+                opacity={0.8}
+                radius={50}
+                maxIntensity={100}
+                gradientSmoothing={10}
+                heatmapMode={'POINTS_DENSITY'}
+              />
+            )}
+          </MapView>
+          <TouchableOpacity
+            onPress={() => {
+              getFirebaseData(setData);
+              Alert.alert(
+                'Data Reloaded',
+                `${JSON.stringify(data) || 'no data'}`,
+              );
+            }}
+            style={styles.button}>
+            <Text>RELOAD DATA getpush:{count}</Text>
+          </TouchableOpacity>
+        </>
       ) : (
         <Text>bekle</Text>
       )}
--- LocationHeatMapper/src/app/screens/HomeScreen/styles.js
+++ LocationHeatMapper/src/app/screens/HomeScreen/styles.js
@@ -1,14 +1,24 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
 export default StyleSheet.create({
   container: {
-    height: '100%',
-    width: '100%',
-    marginTop: 10,
+    flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
   map: {
-    height: '100%',
-    width: '100%',
+    // height: height,
+    // width: width,
+    ...StyleSheet.absoluteFillObject,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 20,
+    height: 50,
+    width: 120,
+    backgroundColor: '#FF6969',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
