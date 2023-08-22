096813198a647426d0a16e01e7e27c26b486ae8e
MuazzezA <ayd.muazzez@gmail.com>
2023-08-22 22:28:13 +0300

    Add: theme

--- LocationHeatMapper/src/assets/theme/theme.js
+++ LocationHeatMapper/src/assets/theme/theme.js
@@ -1,10 +1,20 @@
+import {Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('window');
 const colors = {
   white: '#ffffff',
   black: '#000000',
+  lightGray: '#d3d3d3',
+  red: '#92000A',
+  green: '#008000',
 };
 
 const sizes = {
   base: 16,
+  title: 24,
+  radius: 12,
+  padding: 20,
+  width: width,
+  height: height,
 };
 
 export {colors, sizes};
