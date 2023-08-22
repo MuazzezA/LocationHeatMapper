10259f19c1b115f8c4d42f2917c16252180e62a3
MuazzezA <ayd.muazzez@gmail.com>
2023-08-22 23:21:22 +0300

    Add : svg

---
+++ LocationHeatMapper/src/assets/svg/index.js
@@ -0,0 +1,6 @@
+import HomeLine from './home-line.svg';
+import HomeFill from './home.svg';
+import ProfileLine from './profile-line.svg';
+import ProfileFill from './profile.svg';
+
+export {HomeLine, HomeFill, ProfileLine, ProfileFill};
